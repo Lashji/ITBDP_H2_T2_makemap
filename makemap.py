d7e1cc4295df943221677157092af50856b4c0f0
Lassi <lassi.palojarvi@outlook.com>
2018-11-05 18:19:04 +0200

    basic func

--- makemap.py
+++ makemap.py
@@ -5,14 +5,12 @@ from io import BytesIO
 import sys
 import math
 
-start_col = float(sys.argv[1])
-start_row = float(sys.argv[2])
-cols = float(sys.argv[1]) / float(sys.argv[3])
-rows = float(sys.argv[2]) / float(sys.argv[4])
 
-print(cols)
-print(rows)
-
+def get_coords():
+    tmp = []
+    for i in range (1, 6):
+        tmp.append(float(sys.argv[i]))
+    return tmp
 
 def deg2num(lat_deg, lon_deg, zoom):
     lat_rad = math.radians(lat_deg)
@@ -22,19 +20,41 @@ def deg2num(lat_deg, lon_deg, zoom):
                                 (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return (xtile, ytile)
 
-
 def main():
     zoom = int(sys.argv[5])
     start_col = float(sys.argv[1])
     start_row = float(sys.argv[2])
+    filename = str(sys.argv[6])
+    process_img(start_col, start_row, zoom, filename)
+    
+def save(mapimg, fn):
+    mapimg.save(fn)
 
-    deg = deg2num(start_col, start_row, zoom)
-    mapimg = Image.new("RBG", (256, 256))
-    imgdata = urllib.request.urlopen(
-        "https://tile.openstreetmap.org/14/{}/{}.png".format(deg[0], deg[1])).read()
-    img = Image.open(BytesIO(imgdata))
-    print(img.size)
-    display(img)
+def process_img(start_col, start_row, zoom, filename):
+    user_coords = get_coords()
+    deg_start = deg2num(user_coords[0], user_coords[1], user_coords[4])
+    deg_end = deg2num(user_coords[2], user_coords[3], user_coords[4])
+    cols = get_rows(deg_start[0], deg_end[0])
+    rows = get_rows(deg_start[1], deg_end[1])
+    mapimg = Image.new("RGB", (cols*256, rows*256))
 
+    start_col = int(start_col)
+    start_row = int(start_row)
+
+    for col in range (start_col, start_col+cols):
+        for row in range(start_row, start_row + rows):
+            imgdata = urllib.request.urlopen(
+                  "https://tile.openstreetmap.org/{}/{}/{}.png".format(zoom, col, row)).read()
+            img = Image.open(BytesIO(imgdata))
+            mapimg.paste(img,((col-start_col)*256, (row-start_row)*256))
+    save(mapimg, filename)
+
+
+
+def get_rows(start_deg, end_deg):
+    
+    return end_deg - start_deg 
 
 main()
+
+
